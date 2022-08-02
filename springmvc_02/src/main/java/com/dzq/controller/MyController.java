a1179dca796b96e563dd702e1be4ab0cf46f26a8
dzq <x_mitsui@163.com>
2022-08-02 10:59:12 +0800

    控制请求方式

--- springmvc_02/src/main/java/com/dzq/controller/MyController.java
+++ springmvc_02/src/main/java/com/dzq/controller/MyController.java
@@ -3,17 +3,15 @@ package com.dzq.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @Author: Ma HaiYang
- * @Description: MircoMessage:Mark_7001
- */
+
 @Controller
 //@RequestMapping("/msb")
 //@RequestMapping("/dzq")
 public class MyController {
-    @RequestMapping("/myCon.do")
+    @RequestMapping(value = "/requestData", method = RequestMethod.GET)
     public String test1() {
         System.out.println("this is firstController");
         return "success";
--- springmvc_02/src/main/webapp/index.jsp
+++ springmvc_02/src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
+<link href="static/css/bbb.css" type="text/css">
 <body>
+<%--<script src="static/js/aaa.js" type="text/javascript"></script>--%>
 <h2>Hello World!</h2>
+<img src="static/img/img.png"/>
+<form action="requestData" method="get">
+    <input type="submit" value="submit">
+</form>
 </body>
 </html>
