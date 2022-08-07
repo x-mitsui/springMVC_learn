bacbf459d3be563cb7da7a741c577b67b51412f1
dzq <x_mitsui@163.com>
2022-08-07 15:41:23 +0800

    springMVC：传递请求域数据的另外两种方式

--- ssm01/src/main/java/com/dzq/controller/ScopeController.java
+++ ssm01/src/main/java/com/dzq/controller/ScopeController.java
@@ -4,13 +4,16 @@ import com.dzq.pojo.User;
 import com.dzq.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ScopeController {
@@ -24,7 +27,7 @@ public class ScopeController {
         req.setAttribute("message","HttpServletRequest");
         req.setAttribute("users",userList);
 
-        // session域传参
+        // session域传参，可以直接访问.../setDataPage.jsp，通过删除cookie测试
         session.setAttribute("message","HttpServletRequest");
         session.setAttribute("users",userList);
 
@@ -34,5 +37,35 @@ public class ScopeController {
         application.setAttribute("users",userList);
 
         return "forward:/setDataPage.jsp";
+    }
+
+    /**
+     *
+     * 利用model对象的addAttribute
+     * 主要是针对请求域传递数据进行了api的封装，（所以redirect不行）
+     * 加了一层，解耦了controller和servlet
+     *
+     * 重定向下，无法使用model传递域中的数据
+     * model中的字符串类型的键值对信息转换为请求参数，转发给目标地址
+     */
+    @RequestMapping("setData2")
+    public String setData2(Model model){
+        List<User> users = userService.findAllUser();
+        model.addAttribute("message","reqMessage");
+        model.addAttribute("users",users);
+        return "/setDataPage.jsp";
+    }
+
+    @RequestMapping("setData3")
+    public ModelAndView setData3(){
+        List<User> users = userService.findAllUser();
+        ModelAndView mv = new ModelAndView();
+        Map<String, Object> model = mv.getModel();
+        model.put("message","reqMessage");
+        model.put("users",users);
+
+        mv.setViewName("setDataPage.jsp");
+
+        return mv;
     }
 }
